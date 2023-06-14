3b62d14ff94c49cc7349afea48466de0f4acfd83
Scott Moss <willscottmoss@gmail.com>
2023-06-14 11:42:22 -0500

    init

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,6 @@
 import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
+import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import Link from 'next/link'
--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { revalidatePath } from 'next/cache'
@@ -9,6 +10,14 @@ export const POST = async () => {
     data: {
       userId: user.id,
       content: 'Write about your day!',
+    },
+  })
+
+  const analysis = await analyze(entry.content)
+  await prisma.analysis.create({
+    data: {
+      entryId: entry.id,
+      ...analysis,
     },
   })
 
